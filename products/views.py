21a40d5c6e5e4b5c5d4fabe4dc996e53d68baaa6
d930379 <you@example.com>
2020-07-11 19:12:47 +0530

    added template and bootstrap

--- products/views.py
+++ products/views.py
@@ -1,10 +1,15 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
 def index(request):
-    return HttpResponse('hello world')
+    products= Product.objects.all();
+    #return HttpResponse('hello world')
+    return render(request,'index.html',{'products':products})
+
 
 def new(request):
+
     return HttpResponse('new page')
 
